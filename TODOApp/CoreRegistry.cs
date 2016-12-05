0346aa2b20ddaa649be8968276fb979c927820ea
brfree <brandon.free@extendhealth.com>
2016-12-05 15:49:10 -0700

    TODOApp: Initial pieces added

---
+++ TODOApp/CoreRegistry.cs
@@ -0,0 +1,19 @@
+﻿using FubuMVC.Core.Registration;
+using StructureMap.Graph;
+
+namespace TODOApp
+{
+    public class CoreRegistry : ServiceRegistry
+    {
+        public CoreRegistry()
+        {
+            Scan(x =>
+            {
+                //scans the calling assembly for items to automatically add to the container that follow the default conventions
+                //the main one to take note of is 'Fubu is IFubu'
+                x.TheCallingAssembly();
+                x.WithDefaultConventions();
+            });
+        }
+    }
+}
---
+++ TODOApp/Endpoints/Home/HomeEndpoint.cs
@@ -0,0 +1,19 @@
+﻿
+using FubuMVC.Core;
+
+namespace TODOApp.Endpoints.Home
+{
+    public class HomeEndpoint
+    {
+        [UrlPattern("")] //overides the default translation of the action to a url (in this case, the get_index() with a default url pattern of "/index" will now have a blank url pattern, effectively turning it into the home page)
+        public HomeViewModel get_index()
+        {
+            return new HomeViewModel { Text = "Hello Fubu 3!!" };
+        }
+    }
+
+    public class HomeViewModel
+    {
+        public string Text { get; set; }
+    }
+}
