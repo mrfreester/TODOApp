5a797632cef6730630f9306d823d119caa45e4be
brfree <brandon.free@extendhealth.com>
2016-12-08 16:09:16 -0700

    List Tasks: Listing Tasks from database

--- TODOApp/Endpoints/Home/HomeEndpoint.cs
+++ TODOApp/Endpoints/Home/HomeEndpoint.cs
@@ -1,31 +1,32 @@
 ﻿
+using System.Collections.Generic;
 using FubuMVC.Core;
 using TODOApp.Models;
 
 namespace TODOApp.Endpoints.Home
 {
+
     public class HomeEndpoint
     {
+        private readonly TaskHelper _helper = new TaskHelper();
+
         [UrlPattern("")] //overides the default translation of the action to a url (in this case, the get_index() with a default url pattern of "/index" will now have a blank url pattern, effectively turning it into the home page)
         public HomeViewModel get_index()
         {
-            TaskHelper helper = new TaskHelper();
-
-            helper.Tasks();
-            return new HomeViewModel { Text = "Hello Fubu 3!!" };
+            return new HomeViewModel { Tasks = _helper.Tasks() };
         }
 
         [UrlPattern("AddTask")]
         public HomeViewModel AddTask()
         {
-            return new HomeViewModel { 
-                Text = "Added a task"
+            return new HomeViewModel {
+                Tasks = _helper.Tasks()
             };
         }
     }
 
     public class HomeViewModel
     {
-        public string Text { get; set; }
+        public List<Task> Tasks { get; set; }
     }
 }
