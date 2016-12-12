239ca0471674bc14ad2f73c86debd33bbd35d01a
brfree <brandon.free@extendhealth.com>
2016-12-12 11:48:28 -0700

    Testing: Added basic unit test for get_index

---
+++ TODOApp.Tests/Endpoint/Home/HomeEndpointTests.cs
@@ -0,0 +1,18 @@
+﻿using FubuTestingSupport;
+using NUnit.Framework;
+using TODOApp.Endpoints.Home;
+
+namespace TODOApp.Tests.Endpoint.Home
+{
+    [TestFixture]
+    public class HomeEndpointTests : InteractionContext<HomeEndpoint>
+    {
+        [Test]
+        public void GetIndexReturnsHomeViewModel()
+        {
+            var result = ClassUnderTest.get_index(new HomeViewModel());
+            result.ShouldNotBeNull();
+            result.ShouldBeOfType<HomeViewModel>();
+        }
+    }
+}
